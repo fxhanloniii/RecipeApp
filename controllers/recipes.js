2718dca368fa16c31d37f457ab70ff54dffade67
fxhanloniii <hanlonfrankiex@gmail.com>
2023-04-08 11:27:28 -0700

    database display

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { Recipe } = require('../models');
 
-router.get('/recipe', (req, res) => {
-    res.render('/index.ejs', { Recipe })
-})
-  
-  router.get('/recipe/new', (req, res) => {
-    res.render('/new.ejs')
-})
 
 router.get('/:cuisine', async (req, res, next) => {
     try {
