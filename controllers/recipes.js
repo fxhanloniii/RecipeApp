d65a63da89d88c75285ef43f9cecedc59b437230
fxhanloniii <hanlonfrankiex@gmail.com>
2023-04-08 11:39:06 -0700

    idk

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,7 +3,11 @@ const router = express.Router()
 const { Recipe } = require('../models')
 
 router.get('/recipe', (req, res) => {
-  res.render('/index.ejs', { Recipe })
+    res.render('/index.ejs', { Recipe })
+  })
+  
+router.get('/recipe/new', (req, res) => {
+    res.render('/new.ejs')
 })
 
 
