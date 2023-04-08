52695edc304ad45bb8db48b8419ccbd2563e94bd
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-04-08 11:49:01 -0400

    basic controller set up

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,20 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const { Recipe } = require('../models');
+const express = require('express')
+const router = express.Router()
+const { Recipe } = require('../models')
 
+router.get('/recipe', (req, res) => {
+  res.render('/index.ejs', { Recipe })
+})
 
+router.get('/recipe/new', (req, res) => {
+  res.render('/new.ejs')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router;
+module.exports = router
