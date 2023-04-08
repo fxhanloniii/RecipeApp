1b49efb18e720d993c178787d47a7525ff6dfba1
fxhanloniii <hanlonfrankiex@gmail.com>
2023-04-08 11:21:59 -0700

    displayed database to page

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,8 +2,30 @@ const express = require('express');
 const router = express.Router();
 const { Recipe } = require('../models');
 
+router.get('/recipe', (req, res) => {
+    res.render('/index.ejs', { Recipe })
+})
+  
+  router.get('/recipe/new', (req, res) => {
+    res.render('/new.ejs')
+})
 
-
+router.get('/:cuisine', async (req, res, next) => {
+    try {
+        console.log(req.params);
+        let { cuisine } = req.params;
+        cuisine = cuisine[0].toUpperCase() + cuisine.substring(1,cuisine.length);
+        // cuisine = cuisine.replaceAll
+        console.log(cuisine);
+        // const recipes = await Recipe.find({ 'cuisine': {$regex:/cuisine/i} });
+        const recipes = await Recipe.find({ 'cuisine': `['${cuisine}']` });
+        //const recipes = await Recipe.find
+        res.render('cuisine.ejs', {recipes: recipes})
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+});
 
 
 
