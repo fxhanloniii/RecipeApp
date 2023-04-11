db708c4dbcbd124500a3bc3602c83bd0708daae6
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-04-11 19:54:26 -0400

    debugged new and edit routes. added all recipes index page and other slight views mods

--- controllers/myRecipes.js
+++ controllers/myRecipes.js
@@ -4,7 +4,7 @@ const { Recipe } = require('../models')
 
 //new route (added by SA)
 router.get('/new', (req, res) => {
-  res.render('myRecipe/new.ejs')
+  res.render('myRecipes/new.ejs')
 })
 
 //post route to post through mongo (added by SA)
@@ -19,55 +19,54 @@ router.post('', async (req, res, next) => {
   }
 })
 
-//edit id route (added by SA)
+//edit id route (added by SA) //have editing recipes on a separate page from originals?? using a user route
 router.get('/:id/edit', async (req, res, next) => {
   try {
     const recipeToBeEdited = await Recipe.findById(req.params.id)
-    console.log(bookToBeEdited)
-    res.render('recipes/edit.ejs', { recipe: recipeToBeEdited })
+    res.render('myRecipes/edit.ejs', { recipe: recipeToBeEdited })
   } catch (err) {
     console.log(err)
     next()
   }
 })
 
-//(added by SA)
-router.put('/:id', async (req, res, next) => {
-  try {
-    const updatedRecipe = await Recipe.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    )
-    // console.log(updatedRecipe);
-    res.redirect(`/recipes/${req.params.id}`)
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-})
+// //(added by SA) // will need to create user route
+// router.put('/:id', async (req, res, next) => {
+//   try {
+//     const updatedRecipe = await Recipe.findByIdAndUpdate(
+//       req.params.id,
+//       req.body
+//     )
+//     // console.log(updatedRecipe);
+//     res.redirect(`/recipes/${req.params.id}`)
+//   } catch (err) {
+//     console.log(err)
+//     next()
+//   }
+// })
 
-// get route (added by SA)
-router.get('/:id/delete', async (req, res, next) => {
-  try {
-    const recipeToBeDeleted = await Recipe.findById(req.params.id)
-    // console.log(recipeToBeDeleted);
-    res.render('recipe/delete.ejs', { recipe: recipeToBeDeleted })
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-})
+// // get route (added by SA) // will need to create user route
+// router.get('/:id/delete', async (req, res, next) => {
+//   try {
+//     const recipeToBeDeleted = await Recipe.findById(req.params.id)
+//     // console.log(recipeToBeDeleted);
+//     res.render('recipe/delete.ejs', { recipe: recipeToBeDeleted })
+//   } catch (err) {
+//     console.log(err)
+//     next()
+//   }
+// })
 
-//delete route (added by SA)
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedItem = await Recipe.findByIdAndDelete(req.params.id)
-    // console.log(deletedItem);
-    res.redirect('/recipe')
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-})
+// //delete route (added by SA) // will need to create user route
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const deletedItem = await Recipe.findByIdAndDelete(req.params.id)
+//     // console.log(deletedItem);
+//     res.redirect('/recipe')
+//   } catch (err) {
+//     console.log(err)
+//     next()
+//   }
+// })
 
 module.exports = router
