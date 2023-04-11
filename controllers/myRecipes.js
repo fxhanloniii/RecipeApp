906c3d07c5f7ca65bb27c15d8f1bf8fe0e7583b0
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-04-11 17:01:22 -0400

    CRUD routes added. views new, edit, index page created

---
+++ controllers/myRecipes.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = express.Router()
+const { Recipe } = require('../models')
+
+//new route (added by SA)
+router.get('/new', (req, res) => {
+  res.render('recipe/new.ejs')
+})
+
+//post route to post through mongo (added by SA)
+router.post('', async (req, res, next) => {
+  try {
+    const newRecipe = await Recipe.create(req.body)
+    console.log(newRecipe)
+    res.redirect('/recipe')
+  } catch (err) {
+    console.log(err)
+    next()
+  }
+})
+
+//edit id route (added by SA)
+router.get('/:id/edit', async (req, res, next) => {
+  try {
+    const recipeToBeEdited = await Recipe.findById(req.params.id)
+    console.log(bookToBeEdited)
+    res.render('recipes/edit.ejs', { recipe: recipeToBeEdited })
+  } catch (err) {
+    console.log(err)
+    next()
+  }
+})
+
+//(added by SA)
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    )
+    // console.log(updatedRecipe);
+    res.redirect(`/recipes/${req.params.id}`)
+  } catch (err) {
+    console.log(err)
+    next()
+  }
+})
+
+// get route (added by SA)
+router.get('/:id/delete', async (req, res, next) => {
+  try {
+    const recipeToBeDeleted = await Recipe.findById(req.params.id)
+    // console.log(recipeToBeDeleted);
+    res.render('recipe/delete.ejs', { recipe: recipeToBeDeleted })
+  } catch (err) {
+    console.log(err)
+    next()
+  }
+})
+
+//delete route (added by SA)
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedItem = await Recipe.findByIdAndDelete(req.params.id)
+    // console.log(deletedItem);
+    res.redirect('/recipe')
+  } catch (err) {
+    console.log(err)
+    next()
+  }
+})
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,54 +5,39 @@ const { Recipe } = require('../models')
 // router.get('/recipe', (req, res) => {
 //     res.render('/index.ejs', { Recipe })
 //   })
-  
+
 // router.get('/recipe/new', (req, res) => {
 //     res.render('/new.ejs')
 // })
 
-
-
-
 router.get('/cuisine/:cuisine', async (req, res, next) => {
-    try {
-        // console.log(req.params);
-        let { cuisine } = req.params;
-        cuisine = cuisine[0].toUpperCase() + cuisine.substring(1,cuisine.length);
-        // cuisine = cuisine.replaceAll
-        // console.log(cuisine);
-        // const recipes = await Recipe.find({ 'cuisine': {$regex:/cuisine/i} });
-        const recipes = await Recipe.find({ 'cuisine': `['${cuisine}']` });
-        //const recipes = await Recipe.find
-        // console.log(recipes);
-        res.render('cuisine.ejs', {recipes: recipes})
-    } catch(err) {
-        console.log(err);
-        next();
-    }
-});
-
+  try {
+    // console.log(req.params);
+    let { cuisine } = req.params
+    cuisine = cuisine[0].toUpperCase() + cuisine.substring(1, cuisine.length)
+    // cuisine = cuisine.replaceAll
+    // console.log(cuisine);
+    // const recipes = await Recipe.find({ 'cuisine': {$regex:/cuisine/i} });
+    const recipes = await Recipe.find({ cuisine: `['${cuisine}']` })
+    //const recipes = await Recipe.find
+    // console.log(recipes);
+    res.render('cuisine.ejs', { recipes: recipes })
+  } catch (err) {
+    console.log(err)
+    next()
+  }
+})
 
 //show route
 router.get('/:id', async (req, res, next) => {
-    try {
-        console.log(req.params)
-        const recipeSelected = await Recipe.findById(req.params.id);
-        console.log(recipeSelected)
-        res.render('show.ejs', {recipe: recipeSelected})
-    } catch(err) {
-        next()
-    }
+  try {
+    console.log(req.params)
+    const recipeSelected = await Recipe.findById(req.params.id)
+    console.log(recipeSelected)
+    res.render('show.ejs', { recipe: recipeSelected })
+  } catch (err) {
+    next()
+  }
 })
 
-
-module.exports = router;
-
-
-
-
-
-
-
-
-
-
+module.exports = router
