12b48f7e85245cd89837b81a37a4c619721020d8
rlegler2012 <rlegler2012@hotmail.com>
2023-04-15 09:57:03 -0700

    updated new route

--- config/practiceSeedData.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-	{ cooking_method: 'to make cut into peices', cuisine: 'chickenPIE', ingredients: 'chicken', prep_time: '5 minutes', recipe_name: 'chickenCool' },
-    { cooking_method: 'make pieces', cuisine: 'cheesePIE', ingredients: 'cheese', prep_time: '8 minutes', recipe_name: 'eatPIE' },
-];
